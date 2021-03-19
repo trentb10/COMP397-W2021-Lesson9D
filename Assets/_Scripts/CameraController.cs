aefe48a0140ba55aa2cdd96724fff60cf52d9273
trentb10 <trentbmin@gmail.com>
2021-03-18 22:02:46 -0400

    Fixed controls for mobile and adjusted map

--- Assets/_Scripts/CameraController.cs
+++ Assets/_Scripts/CameraController.cs
@@ -6,10 +6,7 @@ public class CameraController : MonoBehaviour
 {
     [Header("Controls")]
     public Joystick joystick;
-    public float horizontalSensitivity;
-    public float verticalSensitivity;
-
-    public float mouseSensitivity = 1000.0f;
+    public float controlSensitivity = 2.0f;
     public Transform playerBody;
 
     private float XRotation = 0.0f;
@@ -23,11 +20,11 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
-        // float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+        // float mouseX = Input.GetAxis("Mouse X") * controlSensitivity;
+        // float mouseY = Input.GetAxis("Mouse Y") * controlSensitivity;
 
-        float mouseX = joystick.Horizontal;
-        float mouseY = joystick.Vertical;
+        float mouseX = joystick.Horizontal * controlSensitivity;
+        float mouseY = joystick.Vertical * controlSensitivity;
 
         XRotation -= mouseY;
         XRotation = Mathf.Clamp(XRotation, -90.0f, 90.0f);
